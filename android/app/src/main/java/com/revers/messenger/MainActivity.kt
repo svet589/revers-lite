4f08486a4cacec37da2d835d7b3ccd10eef919d2
svet589 <d99428850@gmail.com>
2026-08-14 22:35:44 +0300

    Create MainActivity.kt

---
+++ android/app/src/main/java/com/revers/messenger/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.revers.messenger
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.revers.messenger.ui.screens.AuthScreen
+import com.revers.messenger.ui.theme.ReversTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ReversTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val authViewModel = hiltViewModel<AuthViewModel>()
+                    AuthScreen(
+                        viewModel = authViewModel,
+                        onAuthSuccess = { /* Переход к чатам */ }
+                    )
+                }
+            }
+        }
+    }
+}
